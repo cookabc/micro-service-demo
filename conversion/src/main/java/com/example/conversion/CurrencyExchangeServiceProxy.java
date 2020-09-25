d0009eec8f2bca4f2225709a21a5197f2f659745
xugang <xugang.song@advance.ai>
2020-09-25 16:28:29 +0800

    use robbin as client load balance

--- conversion/src/main/java/com/example/conversion/CurrencyExchangeServiceProxy.java
+++ conversion/src/main/java/com/example/conversion/CurrencyExchangeServiceProxy.java
@@ -1,5 +1,6 @@
 package com.example.conversion;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
  * @author: Xugang Song
  * @create: 2020/9/25
  */
-@FeignClient(name = "conversion-service", url = "localhost:8000")
+@FeignClient(name = "conversion-service")
+@RibbonClient(name="conversion-service")
 public interface CurrencyExchangeServiceProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
